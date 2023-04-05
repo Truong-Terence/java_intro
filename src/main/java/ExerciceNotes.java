3ecf3f8108d2005f98e6932b1981a058265ada76
Truong <tvn.terence@gmail.com>
2023-04-05 14:46:05 +0200

    Class + constructor

--- src/main/java/ExerciceNotes.java
+++ src/main/java/ExerciceNotes.java
@@ -8,21 +8,60 @@ public class ExerciceNotes {
     //Veuillez ensuite afficher la note de chaque élèves
     //Projet à reprendre  => Le transformer avec une (ou plusieurs classes si besoin..)
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Map<String, String> map = new HashMap<String, String>();
-        while (true) {
-            System.out.println("Mark ? ");
-            int mark = scanner.nextInt();
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        Map<String, String> map = new HashMap<String, String>();
+//        while (true) {
+//            System.out.println("Mark ? ");
+//            int mark = scanner.nextInt();
+//
+//            if (mark == -1 ) {
+//                break;
+//            } else {
+//                System.out.println("Name ? ");
+//                String name = scanner.next();
+//                map.put(String.valueOf(mark), name);
+//            }
+//        }
+//            System.out.println(map);
+//    }
+public static void main(String[] args) {
+    Student[] students = new Student[100];
 
-            if (mark == -1 ) {
-                break;
-            } else {
-                System.out.println("Name ? ");
-                String name = scanner.next();
-                map.put(String.valueOf(mark), name);
-            }
+    Scanner scanner = new Scanner(System.in);
+    for (int i = 0; i < students.length; i++) {
+        System.out.println("Saisir prénom :");
+        String firstname = scanner.next();
+        if (firstname.equals("-1")) {
+            break;
         }
-            System.out.println(map);
+
+        System.out.println("Saisir nom :");
+        String name = scanner.next();
+        if (name.equals("-1")) {
+            break;
+        }
+        System.out.println("Saisir age :");
+        short age = scanner.nextShort();
+        if (age == -1) {
+            break;
+        }
+        System.out.println("Saisir note :");
+        int mark = scanner.nextInt();
+        if (mark == -1) {
+            break;
+        }
+        students[i] = new Student(firstname, name, mark, age);
+    }
+    display(students);
+}
+
+    static void display(Student[] students) {
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] == null) {
+                break;
+            }
+            System.out.println(students[i].firstName + " --> " + students[i].mark);
+        }
     }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,15 @@
 public class Student {
-    Student
+
+        String firstName;
+        String name;
+        short age;
+
+        int mark;
+
+    public Student(String firstName, String name, int mark, short age) {
+        this.firstName = firstName;
+        this.name = name;
+        this.age = age;
+        this.mark = mark;
+    }
 }
